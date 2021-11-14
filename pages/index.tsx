4582f3770380573a3d837e84c2a95c287fd85462
MichaelArtemev <68643663+MichaelArtemev@users.noreply.github.com>
2021-11-14 15:40:22 +0300

    create rating comp and compile test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { Htag, Button, P }from "../components";
+import { Htag, Button, P, Rating }from "../components";
+import { useState } from 'react';
 
 export default function Home():JSX.Element {
+  const [rating, setRating] = useState<number>(0);
+
   return (
     <div className={styles.container}>
       <Htag tag="h1">text</Htag>
       <Button appearance="primary">Drop</Button>
       <P>text</P>
+      <Rating rating={rating} isEditable setRating={setRating} />
     </div>
   );
 }
